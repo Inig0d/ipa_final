997e03c452ecc204cb9325d5b98add156ca20689
Jesus-AD <125573653+Jesus-AD@users.noreply.github.com>
2024-11-07 12:57:36 +0100

    Create load_html_test.dart

---
+++ load_html_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+void main() {
+  test('Load HTML content from assets', () async {
+    try {
+      String fileContent = await rootBundle.loadString('assets/html/mi_carnet_vip_canjear.html');
+      expect(fileContent.isNotEmpty, true, reason: 'HTML content should not be empty');
+      print("HTML content loaded successfully: $fileContent");
+    } catch (e) {
+      fail("Error loading HTML file: $e");
+    }
+  });
+}
